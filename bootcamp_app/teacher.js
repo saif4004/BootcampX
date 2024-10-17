e3d791d0cf0584c05ba66a52d735a9d15f7641c9
Saif Al-saedi <saif4004@gmail.com>
2024-10-16 18:13:57 -0700

    Parameterized Query

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -7,18 +7,17 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
+
+const queryString =  `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+`;
+pool.query(queryString, values);
